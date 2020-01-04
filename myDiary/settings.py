7423d8098a642cc831f662f1952c00d5fe73a00b
Ghazi Sultan <ghazisultan25@gmail.com>
2020-01-04 12:40:58 +0530

    added media and static path in settings

--- myDiary/settings.py
+++ myDiary/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILE_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
@@ -136,4 +137,4 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR,'sent_emails')
 ACCOUNT_ACTIVATION_DAYS = 7
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'accounts/media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
